7db09a3128a5096dd09050f3cd99b26fbc7a45e3
Keyth05 <keithyaguana@gmail.com>
2025-02-01 22:40:13 -0500

    feat: :sparkles: Modificated logic for receptor with topic

--- product-category-service/index.js
+++ product-category-service/index.js
@@ -3,6 +3,7 @@ const { connectToMongo } = require('./connection');
 require('dotenv').config();
 
 const brokerUrl = process.env.BROKER_URL;
+const topic = 'categories_products';
 let ws;
 
 function connectWebSocket() {
@@ -10,15 +11,22 @@ function connectWebSocket() {
 
     ws.on('open', () => {
         console.log('Connection to broker established.');
+
+        const subscribeMessage = {
+            event: 'subscribe',
+            topic: topic,
+        };
+        ws.send(JSON.stringify(subscribeMessage));
+        console.log(`Subscribed to topic: ${topic}`);
     });
 
     ws.on('message', async (message) => {
-        console.log("Message received from broker:", message); 
+        console.log("Message received from broker:", message);
         try {
             const event = JSON.parse(message);
             console.log("Parsed message:", event);
 
-            if (event.event === 'delete_category') {
+            if (event.topic === topic && event.event === 'delete_category') {
                 const categoryId = event.category_id;
                 console.log(`Delete products associated with category: ${categoryId}`);
 
