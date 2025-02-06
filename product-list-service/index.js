00b808edb21a6cfd6f88c108b9c0f3b427366976
keyth05 <keithyaguana@gmail.com>
2025-02-05 20:54:36 -0500

    feat: :sparkles: Add health check

--- product-list-service/index.js
+++ product-list-service/index.js
@@ -26,6 +26,10 @@ const resolvers = {
 
 const app = express();
 
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+  });
+
 async function startServer() {
     const server = new ApolloServer({
         typeDefs,
